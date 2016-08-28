d2c3231e96d45fefdd09c2c072296328f1fbca59
SantiFdezM <santiatlas11@hotmail.com>
2016-08-28 07:41:52 +0000

    Agregue conexion desde windows azure

--- VendeAgroWeb/Models/DatabaseModel.Context.cs
+++ VendeAgroWeb/Models/DatabaseModel.Context.cs
@@ -28,7 +28,6 @@ namespace VendeAgroWeb.Models
         public virtual DbSet<Anuncio> Anuncios { get; set; }
         public virtual DbSet<Anuncio_Beneficio> Anuncio_Beneficio { get; set; }
         public virtual DbSet<Anuncio_Paquete> Anuncio_Paquete { get; set; }
-        public virtual DbSet<Anuncio_Plazo> Anuncio_Plazo { get; set; }
         public virtual DbSet<Banner> Banners { get; set; }
         public virtual DbSet<Beneficio> Beneficios { get; set; }
         public virtual DbSet<Categoria> Categorias { get; set; }
@@ -40,8 +39,6 @@ namespace VendeAgroWeb.Models
         public virtual DbSet<Pago> Pagoes { get; set; }
         public virtual DbSet<Pago_Concepto> Pago_Concepto { get; set; }
         public virtual DbSet<Paquete> Paquetes { get; set; }
-        public virtual DbSet<Plazo> Plazoes { get; set; }
-        public virtual DbSet<PlazoBase> PlazoBases { get; set; }
         public virtual DbSet<Subcategoria> Subcategorias { get; set; }
         public virtual DbSet<Usuario> Usuarios { get; set; }
         public virtual DbSet<Usuario_Administrador> Usuario_Administrador { get; set; }
