6255f823290c9541ac2c6fe2259559419c06845e
EstefaniaMoralesA <a01327969@gmail.com>
2016-09-04 17:38:48 -0500

    menu

---
+++ VendeAgroWeb/Controllers/Pagina/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VendeAgroWeb.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OfertasDelDia()
+        {
+            return View();
+        }
+
+        public ActionResult Anunciate()
+        {
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+    }
+}
---
+++ VendeAgroWeb/Models/Pagina/PaginaViewModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VendeAgroWeb.Models.Pagina
+{
+    public class HomeViewModel
+    {
+        public IList<Categoria> Categorias { get; set; }
+        public IList<Subcategoria> Subcategorias { get; set; }
+        public IList<Anuncio> AnunciosDestacados { get; set; }
+    }
+
+    public class BusquedaViewModel
+    {
+        public IList<Anuncio> FiltroTextoAnuncios { get; set; }
+    }
+
+    public class OfertasDelDiaViewModel
+    {
+    }
+
+    public class AnunciateViewModel
+    {
+    }
+
+    public class ContactoViewModel
+    {
+    }
+}
