2def264498da9f469443d34ee3e0622e4f5643b5
EstefaniaMoralesA <a01327969@gmail.com>
2016-09-18 21:39:55 -0500

    Administrador layout

---
+++ VendeAgroWeb/VendeAgroWeb/Models/Administrador/AdministradorViewModels.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VendeAgroWeb.Models.Administrador
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Campo requerido")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Introduzca un email válido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+    }
+
+    public class OlvidoContrasenaViewModel
+    {
+        [Required(ErrorMessage = "Campo requerido")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Introduzca un email válido")]
+        public string Email { get; set; }
+    }
+
+    public class CambiarContrasenaViewModel
+    {
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(20, ErrorMessage = "La contraseña debe contener como mínimo 6 caracteres", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "La contraseña y su confirmacion no son iguales.")]
+        [Display(Name = "ConfirmaContraseña")]
+        public string ConfirmaPassword { get; set; }
+    }
+
+    public class AnunciosViewModel {
+    }
+}
