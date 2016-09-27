98b5f350174afab55da0b2d3d125c2c197cef40f
EstefaniaMoralesA <a01327969@gmail.com>
2016-09-27 16:36:33 -0500

    carrusel

--- VendeAgroWeb/VendeAgroWeb/App_Start/BundleConfig.cs
+++ VendeAgroWeb/VendeAgroWeb/App_Start/BundleConfig.cs
@@ -17,6 +17,9 @@ namespace VendeAgroWeb
             bundles.Add(new ScriptBundle("~/bundles/nouislider").Include(
                         "~/Scripts/nouislider.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/owlcarousel").Include(
+                        "~/Scripts/owl.carousel.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,6 +28,12 @@ namespace VendeAgroWeb
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/owlcarousel").Include(
+                      "~/Content/owl.carousel.css",
+                      "~/Content/owl.theme.css",
+                      "~/Content/owl.transitions.css"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
