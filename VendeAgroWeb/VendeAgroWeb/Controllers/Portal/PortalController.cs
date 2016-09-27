081255b7e4ffd2bb0f756eed7c6d99ffe8225a80
SantiFdezM <santiatlas11@hotmail.com>
2016-09-27 19:57:48 +0000

    Minor fix

--- VendeAgroWeb/VendeAgroWeb/Controllers/Portal/PortalController.cs
+++ VendeAgroWeb/VendeAgroWeb/Controllers/Portal/PortalController.cs
@@ -18,12 +18,7 @@ namespace VendeAgroWeb.Controllers.Administrador
         // GET: Usuario_Administrador
         public async Task<ActionResult> Index()
         {
-            if (await Startup.GetAplicacionUsuariosManager().VerificarAdminSesionAsync() == LoginStatus.Incorrecto)
-            {
-                return RedirectToAction("Login", "Administrador");
-            }
-            
-            return View(Startup.GetAplicacionUsuariosManager().getUsuarioAdministradorActual(Request));
+            return View();
         }
 
         public ActionResult Registro()
