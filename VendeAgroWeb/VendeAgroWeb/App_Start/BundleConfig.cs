70d52c7a707cc5241111064f1cbb245eca25b790
EstefaniaMoralesA <a01327969@gmail.com>
2017-05-07 18:08:33 -0500

    refactor

--- VendeAgroWeb/VendeAgroWeb/App_Start/BundleConfig.cs
+++ VendeAgroWeb/VendeAgroWeb/App_Start/BundleConfig.cs
@@ -32,6 +32,9 @@ namespace VendeAgroWeb
             bundles.Add(new ScriptBundle("~/bundles/lightGallery/js").Include(
                        "~/Scripts/lightGallery.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/telephone/js").Include(
+                       "~/Scripts/telephone.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
